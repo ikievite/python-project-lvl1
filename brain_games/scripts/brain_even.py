114dd45ff773015caf24c85f93feefca4475dc29
ikievite <ikievite@gmail.com>
2020-05-20 22:37:52 +0300

    modify logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,14 +4,17 @@
 """Check for Even."""
 
 
-from brain_games.cli import check_even
+from brain_games.cli import welcome_user
+from brain_games.games.even import even_logic
 
 
 def main():
     """Func responses for main logic."""
     print('\nWelcome to the Brain Games!')  # noqa: WPS421
     print('Answer "yes" if number even otherwise answer "no".\n')  # noqa: WPS421
-    check_even()
+    name = welcome_user()
+    print(f'Hello, {name}!')
+    even_logic(name)
 
 
 if __name__ == '__main__':
