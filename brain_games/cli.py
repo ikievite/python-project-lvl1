1e5e1dd9a69e999f5e8b5d6d31f009db387fa0ab
ikievite <ikievite@gmail.com>
2020-06-03 21:45:27 +0300

    add function engine() that response for interaction with gamer

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -33,3 +33,31 @@ def check_answer(answer_right, username):
         output = f"""'{answer}' is wrong answer ;(. Correct answer was '{right}'.
 Let's try again, {username}!"""
     return output
+
+
+def engine(description, entries, number_games):
+    """Make for game logic.
+
+    Args:
+        description: of game
+        entries: list with tuples, tuple contain question and right answer
+        number_games: number of games that user can execute
+    """
+    print(description)
+    name = welcome_user()
+    count = 0
+    for i in entries:
+        question, right_answer = i
+        if isinstance(right_answer, int):
+            answer = prompt.integer(question)
+        elif isinstance(right_answer, str):
+            answer = prompt.string(question)
+        if answer == right_answer:
+            print('Correct!')
+            count += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+    if count == number_games:
+        print(f'Congratulations, {name}')
