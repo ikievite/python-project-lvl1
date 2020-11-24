2292cae2f5569c302b28230c54232470ac096ab1
ikievite <ikievite@gmail.com>
2020-11-24 14:41:47 +0200

    change names for ranges

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,31 +11,31 @@ from brain_games.cli import engine
 def prepare_progression_game():
     """Generate questions and right answers.
 
-    Args:
-        min_element: min element of progression
-        max_element: max element of progression
-
     Returns:
         question: for question
         correct: right answer
     """
-    min_element = 1
-    max_element = 10
-    start = random.randint(min_element, max_element)
-    step = random.randint(min_element, max_element)
-    hidden = random.randint(min_element, max_element)
+    min_start_element = 1
+    max_start_element = 10
+    min_step_element = 1
+    max_step_element = 10
+    min_hidden_element = 1
+    max_hidden_element = 10
+    start = random.randint(min_start_element, max_start_element)
+    step = random.randint(min_step_element, max_step_element)
+    hidden = random.randint(min_hidden_element, max_hidden_element)
     i = 1
     question = ''
-    while i <= max_element:
+    while i <= max_hidden_element:
         if i == hidden:
             question = question + ' ..'
-            correct = start
+            right_answer = start
         else:
             question = question + ' ' + str(start)
         start += step
         i += 1
     question = question.strip()
-    return question, str(correct)
+    return question, str(right_answer)
 
 
 def run_progression():
